54de45500cb1238deda2a3f1add605efcf14d0f9
David Bebb <davidbebb@gmail.com>
2015-11-19 21:50:14 +0000

    annotated hello world to highlight concepts here

--- hello_world.fs
+++ hello_world.fs
@@ -1,6 +1,8 @@
-open System
-[<EntryPoint>]
-let main (argv :String[]) =
-  printfn "Hello World"
-  Console.ReadLine() |> ignore
-  0
+open System //This allows us to use Console. later on
+//This is a comment in f#
+[<EntryPoint>] //This tells the compiler/repl where to start
+let main (argv :String[]) = // NB could have left this as 'let main argv =', as f# can guess type...'
+  printfn "Hello World" //Simples
+  Console.ReadLine() |> ignore // F# rule: function MUST have a return. |> ignore as a way of discarding the
+  // returm from .readline
+  0 // The return from this function  (did I mention that f# likes functions to return stuff)
